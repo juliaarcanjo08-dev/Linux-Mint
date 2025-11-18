35651ecfeb6d1a9e0b3682a89f12085b650238d1
Anna Julia Arcanjo <juliaarcanjo08@gmail.com>
2025-11-17 21:55:32 -0300

    Atualização

---
+++ comandos/comandos.sh
@@ -0,0 +1,61 @@
+   1  sudo apt update
+    2  sudo apt upgrade 
+    3  sudo apt upgrade --fix-mssing
+    4  sudo apt upgrade --fix-missing
+    5  exit
+    6  poweroff
+    7  sudo apt update
+    8  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg~
+    9  sudo apt install curl apt-transport-https
+   10  curl -fsS https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo tee /etc/apt/trusted.gpg.d/brave.asc
+   11  sudo apt install brave-browser
+   12  sudo apt upload
+   13  sudo apt update
+   14  sudo apt install midori
+   15  sudo snap install midori
+   16  sudo apt install snapd
+   17  sudo add-apt-repository ppa:midori/midori-dev
+   18  sudo apt-get update
+   19  sudo apt-get install midori
+   20  sudo apt update
+   21  sudo apt --fix-broken install
+   22  sudo clean
+   23  $ wget https://dist.torproject.org/torbrowser/14.0.3/tor-browser-linux-x86_64-14.0.3.tar.xz
+   24  sudo apt update && sudo apt upgrade -y
+   25  cd ~  # Mudar para o diretório home (ou onde você deseja salvar o arquivo)
+   26  wget https://dist.torproject.org/torbrowser/12.5.0/tor-browser-linux64-12.5.0_en-US.tar.xz
+   27  tar -xvJf tor-browser-linux64-12.5.0_en-US.tar.xz
+   28  sudo apt update && sudo apt upgrade -y
+   29  sudo apt-get
+   30  cd ~  # Mudar para o diretório home (ou onde você deseja salvar o arquivo)
+   31  wget https://dist.torproject.org/torbrowser/12.5.0/tor-browser-linux64-12.5.0_en-US.tar.xz
+   32  cd ~  # Mudar para o diretório home (ou onde você deseja salvar o arquivo)
+   33  wget https://dist.torproject.org/torbrowser/12.5.0/tor-browser-linux64-12.5.0_en-US.tar.xz
+   34  sudo apt update && sudo apt upgrade -y
+   35  sudo apt-get update
+   36  wget https://dist.torproject.org/torbrowser/12.5.0/tor-browser-linux64-12.5.0_en-US.tar.xz
+   37  curl -O https://dist.torproject.org/torbrowser/12.5.0/tor-browser-linux64-12.5.0_en-US.tar.xz
+   38  sudo apt update
+   39  sudo apt install git vim pip phyton3 
+   40  git clone 
+   41  sudo apt install git
+   42  git clone 
+   43  git config --global user.name 
+   44  git config --global user.name 
+   45  git config --global user.email 
+   46  git config --global core.editor vim
+   47  git config --global merge.tool vimdiff
+   48  git config --global color.ui true
+   49  git config --list
+   50  sudo apt update
+   51  notepadqq
+   52  sudo apt install notepadqq
+   53  sudo apt install vim
+   54  sudo apt install torbrowser-launcher 
+   55  sudo apt install vim
+   56  sudo apt install git
+   57  wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/11087/wps-office_11.2.0.11087_amd64.deb
+   58  sudo apt install ./wps-office_11.2.0.11087_amd64.deb
+   59  git history
+   60  git--history
+   61  history
